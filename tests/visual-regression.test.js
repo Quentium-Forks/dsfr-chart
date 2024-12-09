b75122bec97dde617f7ac8c44dba67edc0d8e3cc
Thomas Weidmann <thomas.weidmann@actimage.com>
2024-12-09 13:30:18 +0100

    Add test for all chart stories

--- tests/visual-regression.test.js
+++ tests/visual-regression.test.js
@@ -1,26 +1,39 @@
-// @ts-check
+// This test require the project to be build and storybook running to work
+
 const { test, expect } = require('@playwright/test')
 const { readdirSync } = require('fs')
+const { describe } = require('node:test')
 
 const stories = readdirSync('src/stories')
   .filter((file) => file.endsWith('.stories.js'))
   .map((file) => ({
     name: file.split('.')[0],
-    path: `/?path=/story/stories-${file.split('.')[0].toLowerCase()}--default`
+    paths: Object.keys(require(`../src/stories/${file}`))
+      .filter((x) => x !== 'default')
+      .map((exported) => ({
+        name: exported,
+        url: `/?path=/story/stories-${file
+          .split('.')[0]
+          .toLowerCase()}--${exported}`
+      }))
   }))
 
-stories.forEach((story) => {
-  test(story.name, async ({ page }) => {
-    await page.goto(story.path)
-    // await page.waitForSelector('#storybook-root')
+describe('Charts visual regression', () => {
+  stories.forEach((story) => {
+    story.paths.forEach((path) => {
+      test(`${story.name} - ${path.name}`, async ({ page }) => {
+        await page.goto(path.url)
+        // await page.waitForSelector('#storybook-root')
 
-    const graph = await page.locator('#storybook-preview-iframe')
+        const graph = await page.locator('#storybook-preview-iframe')
 
-    await expect(graph).toHaveScreenshot(
-      `${story.name.toLowerCase()}-${process.platform}.png`,
-      {
-        animations: 'disabled'
-      }
-    )
+        await expect(graph).toHaveScreenshot(
+          `${story.name.toLowerCase()}-${path.name.toLowerCase()}-${process.platform}.png`,
+          {
+            animations: 'disabled'
+          }
+        )
+      })
+    })
   })
 })
