32d85d032006032110ca08740b9b752031a3b47a
QuentiumYT <quentin.lienhardt@gmail.com>
2024-12-17 14:51:27 +0100

    Set marianne back

--- src/utils/global.js
+++ src/utils/global.js
@@ -1136,6 +1136,7 @@ export const getHexaFromToken = function (token, theme) {
 };
 
 export const configureChartDefaults = () => {
+  Chart.defaults.font.family = 'Marianne';
   Chart.defaults.font.size = 12;
   Chart.defaults.font.lineHeight = 1.66;
   Chart.defaults.color = '#666666';
