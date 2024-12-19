5d16820d4ae1df2c178a534fa90b8d59aade7c19
QuentiumYT <quentin.lienhardt@gmail.com>
2024-12-19 16:19:12 +0100

    Kickstart render maps & fix build process env

--- src/components/MapChart.vue
+++ src/components/MapChart.vue
@@ -259,6 +259,8 @@ export default {
     this.prefixClass = 'FR-' + this.level + '-';
   },
   mounted() {
+    this.createChart();
+
     const element = document.documentElement;
     element.addEventListener('dsfr.theme', (e) => {
       if (this.chartId !== '') {
