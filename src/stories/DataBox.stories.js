25565bad18f0622314610abf60b473e984960bf8
QuentiumYT <quentin.lienhardt@gmail.com>
2025-01-13 12:10:29 +0100

    Add table

--- src/stories/DataBox.stories.js
+++ src/stories/DataBox.stories.js
@@ -26,13 +26,26 @@ export const Default = {
     modalContent: 'Contenu de la modale'
   },
   render: (args) => html`
-    <data-box id="${args.id}" ${Object.entries(args).map(([k, v]) => `${k}="${v}"`).join(' ')}>
+    <data-box
+      id="${args.id}"
+      ${Object.entries(args).map(([k, v]) => `${k}="${v}"`).join(' ')}>
     </data-box>
-    <pie-chart databox-id="${args.id}" databox-type="chart"
-    x='[["Non-salariés", "Emplois à durée indéterminée", "Contrats à durée déterminée", "Apprentis", "Intérimaires"]]'
-    y="[[11.7, 74.8, 9.3, 1.6, 2.6]]"
-    name='["Non-salariés", "Emplois à durée indéterminée", "Contrats à durée déterminée", "Apprentis", "Intérimaires"]'
-    unit-tooltip="%"></pie-chart>
+    <pie-chart
+      databox-id="${args.id}"
+      databox-type="chart"
+      x='[["Non-salariés", "Emplois à durée indéterminée", "Contrats à durée déterminée", "Apprentis", "Intérimaires"]]'
+      y="[[11.7, 74.8, 9.3, 1.6, 2.6]]"
+      name='["Non-salariés", "Emplois à durée indéterminée", "Contrats à durée déterminée", "Apprentis", "Intérimaires"]'
+      unit-tooltip="%">
+    </pie-chart>
+    <table-chart
+      databox-id="def"
+      databox-type="table"
+      x='["Non-salariés", "Emplois à durée indéterminée", "Contrats à durée déterminée", "Apprentis", "Intérimaires"]'
+      y="[[11.7, 74.8, 9.3, 1.6, 2.6]]"
+      table-name="Nomenclature"
+      name='["Non-salariés", "Emplois à durée indéterminée", "Contrats à durée déterminée", "Apprentis", "Intérimaires"]'>
+    </table-chart>
   `
 };
 
