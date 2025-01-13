3dbe71f525f136a0a11d3a02deeb80f903fdce13
Thomas Weidmann <thomas.weidmann@actimage.com>
2025-01-13 16:55:14 +0100

    WIP doc page proposal

--- src/main.js
+++ src/main.js
@@ -3,3 +3,7 @@ import '../node_modules/@gouvfr/dsfr/dist/dsfr.main.min.css';
 import '../node_modules/@gouvfr/dsfr/dist/utility/utility.main.min.css';
 
 import '@/charts/main.js';
+import { createApp } from 'vue';
+import Documentation from './components/Documentation.vue';
+
+ createApp(Documentation).mount('#root')
