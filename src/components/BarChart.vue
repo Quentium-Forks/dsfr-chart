02ef61368c474ba3f39ad1e64c4eb887e85bebae
QuentiumYT <quentin.lienhardt@gmail.com>
2025-01-13 14:17:01 +0100

    Clean SCSS

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -387,7 +387,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 @import '@/styles/Tooltip.scss';
 @import '@/styles/Rcol.scss';
 @import '@/styles/WidgetContainer.scss';
