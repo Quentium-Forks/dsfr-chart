7af4abc0c0d671828dc10a2c9c847488576a628f
QuentiumYT <quentin.lienhardt@gmail.com>
2025-01-13 12:09:28 +0100

    Begin fix storybook

--- src/stories/DataBox.stories.js
+++ src/stories/DataBox.stories.js
@@ -1,14 +1,39 @@
+import { html } from 'lit';
+
 const meta = {
-  component: 'data-box'
+  component: 'data-box',
+  subcomponents: {
+    PieChart: 'pie-chart',
+  }
 };
 
 export default meta;
 
 export const Default = {
   args: {
-    dataBoxTitle: 'Titre de votre Databox',
-    dataBoxDescription: 'Ceci est une nouvelle description pour la databox.',
-    dataBoxDate: '2024-10-22',
-    unitTooltip: '%'
-  }
+    id: 'def',
+    title: 'Statut des emplois en France en 2018',
+    tooltipTitle: 'Statut des emplois en France',
+    tooltipContent: "Pour l'année 2018. France hors Mayotte, population des ménages, personnes en emploi.",
+    source: 'Insee, enquête Emploi',
+    date: '04/11/2024',
+    trend: '-15%',
+    segmentedControl: true,
+    screenshot: true,
+    download: true,
+    fullscreen: true,
+    modalTitle: 'Titre de la modale',
+    modalContent: 'Contenu de la modale'
+  },
+  render: (args) => html`
+    <data-box id="${args.id}" ${Object.entries(args).map(([k, v]) => `${k}="${v}"`).join(' ')}>
+    </data-box>
+    <pie-chart databox-id="${args.id}" databox-type="chart"
+    x='[["Non-salariés", "Emplois à durée indéterminée", "Contrats à durée déterminée", "Apprentis", "Intérimaires"]]'
+    y="[[11.7, 74.8, 9.3, 1.6, 2.6]]"
+    name='["Non-salariés", "Emplois à durée indéterminée", "Contrats à durée déterminée", "Apprentis", "Intérimaires"]'
+    unit-tooltip="%"></pie-chart>
+  `
 };
+
+console.log(Object.entries(Default.args).map(([k, v]) => `${k}="${v}"`).join(' '));
