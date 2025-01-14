f930e78f575c61a89097ed1a039520f952f86d0f
QuentiumYT <quentin.lienhardt@gmail.com>
2025-01-14 14:01:09 +0100

    Fix type date arg

--- src/stories/DataBox.stories.js
+++ src/stories/DataBox.stories.js
@@ -13,8 +13,13 @@ const meta = {
     modalContent: 'Contenu de la modale',
     id: 'storybook-databox',
     date: '04/11/2024',
-    trend: '-15%',
+    trend: '-15%'
   },
+  argTypes: {
+    date: {
+      control: 'text'
+    }
+  }
 };
 
 export default meta;
@@ -24,7 +29,7 @@ export const SinglePieChart = {
     title: 'Statut des emplois en France en 2018',
     tooltipTitle: 'Statut des emplois en France',
     tooltipContent: "Pour l'année 2018. France hors Mayotte, population des ménages, personnes en emploi.",
-    source: 'Insee, enquête Emploi',
+    source: 'Insee, enquête Emploi'
   },
   render: (args) => html`
     <data-box
@@ -69,7 +74,7 @@ export const MultipleScatterCharts = {
     title: 'Emplois en France de 1926 à 1950',
     tooltipTitle: 'Emplois en France',
     tooltipContent: "Nombre d'emplois en France de 1926 à 1950, par genre. Se base sur les données de l'INSEE et de Pôle Emploi et d'une autre source.",
-    source: 'INSEE, Pôle Emploi, Autre source',
+    source: 'INSEE, Pôle Emploi, Autre source'
   },
   render: (args) => html`
     <data-box
