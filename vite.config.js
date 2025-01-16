39f81c610323b972efd778df5745bc1bda507eab
Thomas Weidmann <thomas.weidmann@actimage.com>
2025-01-16 10:03:28 +0100

    Fix native custom element warning

--- vite.config.js
+++ vite.config.js
@@ -5,25 +5,47 @@ import path from 'path';
 export default {
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
-    outDir: 'docs',
+    outDir: "docs",
     rollupOptions: {
       output: {
-        entryFileNames: '[name].min.js',
+        entryFileNames: "[name].min.js",
         assetFileNames: (entry) => {
           if (entry.names[0].match(/\.svg$/)) {
-            return 'img/[name].min[extname]';
+            return "img/[name].min[extname]";
           } else if (entry.names[0].match(/\.(ttf|otf|woff|woff2)?$/)) {
-            return 'fonts/[name][extname]';
+            return "fonts/[name][extname]";
           }
 
-          return '[name][extname]';
+          return "[name][extname]";
         },
       },
     },
   },
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) =>
+            [
+              "data-box",
+              "bar-chart",
+              "bar-line-chart",
+              "gauge-chart",
+              "line-chart",
+              "map-chart-reg",
+              "map-chart",
+              "multiline-chart",
+              "pie-chart",
+              "radar-chart",
+              "scatter-chart",
+              "table-chart",
+            ].includes(tag),
+        },
+      },
+    }),
+  ],
 };
