526e4e8bd017331a4b873b243152823d828b76ae
QuentiumYT <quentin.lienhardt@gmail.com>
2025-01-27 15:18:56 +0100

    Supprimer la majuscule à “Points d’indice”

--- src/assets/data.js
+++ src/assets/data.js
@@ -16,7 +16,7 @@ export const chartData = {
       y: '[51.50, 55.30, 61.50, 70.20, 81.10, 92.60, 100.20, 104.60, 96.90, 98.00, 104.90, 106.80, 104.70, 102.70, 100.20, 100.40, 102.90, 106.00, 109.10, 114.60]',
       name: 'Indices des prix des logements anciens',
       selectedPalette: 'defaultColor',
-      unitTooltip: 'Points d’indice',
+      unitTooltip: 'points d’indice',
     },
     multiLine: {
       x: '[[1975, 1980, 1985, 1990, 1995, 2000, 2005, 2010, 2015, 2020], [1975, 1980, 1985, 1990, 1995, 2000, 2005, 2010, 2015, 2020]]',
