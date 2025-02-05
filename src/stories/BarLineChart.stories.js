ed228e2edf67dc9141142c2cb23a29ffefbdfcbb
QuentiumYT <quentin.lienhardt@gmail.com>
2025-02-05 15:26:41 +0100

    Full storybook stories rewrite using data

--- src/stories/BarLineChart.stories.js
+++ src/stories/BarLineChart.stories.js
@@ -1,17 +1,9 @@
-const meta = {
-  component: 'bar-line-chart'
+import { chartData } from '../assets/data';
+
+export default {
+  component: 'bar-line-chart',
 };
 
-export default meta;
-
 export const Default = {
-  args: {
-    x: '[1980, 1990, 2000, 2010]',
-    yBar: '[826.1, 793.1, 807.4, 832.8]',
-    yLine: '[14.9, 13.4, 13.1, 12.9]',
-    nameBar: 'Taux de natalité',
-    nameLine: 'Naissances',
-    unitTooltipBar: 'milliers',
-    unitTooltipLine: '%'
-  }
+  args: chartData.barLineChart.default,
 };
