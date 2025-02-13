4838aab45ad188264cf18b5f8bb646105de522a1
QuentiumYT <quentin.lienhardt@gmail.com>
2025-02-13 16:41:17 +0100

    Add missing import

--- src/components/doc/examples.js
+++ src/components/doc/examples.js
@@ -7,6 +7,7 @@ import MapChartReg from '@/components/MapChartReg.vue';
 import PieChart from '@/components/PieChart.vue';
 import RadarChart from '@/components/RadarChart.vue';
 import ScatterChart from '@/components/ScatterChart.vue';
+import { chartData } from '@/assets/data.js';
 
 export const chartExamples = [
   // Graphique en ligne
