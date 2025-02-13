846d8a829cc385c138b1d6b1484dd2dd60e138f6
QuentiumYT <quentin.lienhardt@gmail.com>
2025-02-13 15:35:35 +0100

    test this shit

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+import path from 'path';
+
 /** @type { import('@storybook/web-components-vite').StorybookConfig } */
 export default {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -6,7 +8,27 @@ export default {
     name: '@storybook/web-components-vite',
   },
   core: {
+    // builder: '@storybook/builder-vite',
     disableTelemetry: true,
     disableWhatsNewNotifications: true,
+  },
+  // async viteFinal(config) {
+  //   return {
+  //     ...config,
+  //     resolve: {
+  //       ...config.resolve,
+  //       alias: {
+  //         ...config.resolve?.alias,
+  //         '@': path.resolve(__dirname, '../src'),
+  //       }
+  //     }
+  //   };
+  // },  
+  viteFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../src'),
+    };
+    return config;
   },
 };
