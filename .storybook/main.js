40cbf09c1eb1ed8a04cdde7d3bb7cbe380366db5
QuentiumYT <quentin.lienhardt@gmail.com>
2025-02-13 16:00:49 +0100

    Revert "test this shit"
    This reverts commit 846d8a829cc385c138b1d6b1484dd2dd60e138f6.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,3 @@
-import path from 'path';
-
 /** @type { import('@storybook/web-components-vite').StorybookConfig } */
 export default {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -8,27 +6,7 @@ export default {
     name: '@storybook/web-components-vite',
   },
   core: {
-    // builder: '@storybook/builder-vite',
     disableTelemetry: true,
     disableWhatsNewNotifications: true,
-  },
-  // async viteFinal(config) {
-  //   return {
-  //     ...config,
-  //     resolve: {
-  //       ...config.resolve,
-  //       alias: {
-  //         ...config.resolve?.alias,
-  //         '@': path.resolve(__dirname, '../src'),
-  //       }
-  //     }
-  //   };
-  // },  
-  viteFinal: async (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, '../src'),
-    };
-    return config;
   },
 };
