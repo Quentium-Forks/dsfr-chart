feee58178c6f28ffd670d54b38c89693cfdf15b4
Gautier Lambla <gautier.lambla@actimage.com>
2025-02-17 09:37:38 +0100

    38196 switch callout to alert

--- src/components/doc/AccessibilitySection.vue
+++ src/components/doc/AccessibilitySection.vue
@@ -3,8 +3,8 @@
     <h2 id="Accessibilité">
       Accessibilité
     </h2>
-    <div class="fr-callout fr-callout--brown-caramel fr-icon-information-line">
-      <p class="fr-callout__text">
+    <div class="fr-alert fr-alert--error fr-mb-5v">
+      <p>
         Utilisés seuls, les graphiques du DSFR Charts sont inaccessibles aux personnes aveugles, déficientes visuelles et aux personnes handicapées motrices. Il conviendra donc d’apporter une alternative textuelle pertinente à chaque fois qu’un graphique est affiché.
       </p>
     </div>
@@ -25,8 +25,8 @@
 
     <h4>Alternative 1 : utiliser un tableau</h4>
     <p>Présenter les résultats sous forme de tableau est sans doute l’option la plus simple, surtout s’il y a beaucoup de données à présenter. On privilégiera dans le cas des contenus les plus complexes la création de plusieurs tableaux simples et non de tableaux avec des cellules fusionnées pour faciliter la lecture.</p>
-    <div class="fr-callout fr-icon-information-line">
-      <p class="fr-callout__text">
+    <div class="fr-alert fr-alert--info fr-mb-6v">
+      <p>
         Le composant databox embarque par défaut une alternative sous forme de tableau.
       </p>
     </div>
@@ -71,11 +71,11 @@
       </a>
       présenté sous le graphique.
     </p>
-    <div class="fr-callout fr-icon-information-line">
-      <h4 class="fr-callout__title">
+    <div class="fr-alert fr-alert--info fr-mb-6v">
+      <h4 class="fr-alert__title">
         Important
       </h4>
-      <p class="fr-callout__text">
+      <p>
         Il conviendra de documenter cette approche dans la déclaration d’accessibilité pour que les personnes handicapées concernées sachent qu’une alternative accessible est systématiquement fournie.
       </p>
     </div>
