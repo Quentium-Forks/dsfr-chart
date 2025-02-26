d06c10b8d95c83ceec84833cc301e18b1887c231
QuentiumYT <quentin.lienhardt@gmail.com>
2025-02-26 10:00:58 +0100

    Revert add BarSubSeries component

--- src/charts/BarSubSeriesChart.js
+++
@@ -1,7 +0,0 @@
-import { defineCustomElement } from 'vue';
-
-import BarSubSeriesChart from '@/components/BarSubSeriesChart.vue';
-
-const BarSubSeriesChartElement = defineCustomElement(BarSubSeriesChart);
-
-customElements.define('bar-sub-series-chart', BarSubSeriesChartElement, { shadowRoot: false });
