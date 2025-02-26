b870f3ac7b566620482bd5ac658024d20ed612a6
QuentiumYT <quentin.lienhardt@gmail.com>
2025-02-26 09:35:07 +0100

    Redo back button

--- src/components/PieChart.vue
+++ src/components/PieChart.vue
@@ -20,16 +20,25 @@
             </div>
           </div>
 
-          <button
-            v-if="isSubLevel"
-            class="fr-btn fr-btn--sm fr-icon-error-line fr-btn--icon-left fr-btn--secondary"
-            @click="resetSub"
+          <div
+            :class="isSubLevel ? '' : 'fr-mt-6v'"
+            style="text-align: center"
           >
-            Réinitialiser
-          </button>
-          <p v-if="subTitle">
-            {{ subTitle }}
-          </p>
+            <button
+              v-if="isSubLevel"
+              class="fr-btn fr-btn--sm fr-icon-arrow-go-back-fill fr-btn--icon-left fr-btn--tertiary-no-outline fr-ml-4w"
+              style="position: absolute; left: 0"
+              @click="resetSub"
+            >
+              Retour
+            </button>
+            <p
+              v-if="subTitle"
+              class="fr-mb-0"
+            >
+              {{ subTitle }}
+            </p>
+          </div>
 
           <canvas :ref="chartId" />
 
