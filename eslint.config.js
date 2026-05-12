c69e1399cb85016897dddaee5b69b57ce92edfc0
QuentiumYT <quentin.lienhardt@gmail.com>
2026-05-12 15:27:21 +0200

    Bump dependencies & improve eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,10 @@
-import globals from 'globals';
 import pluginJs from '@eslint/js';
 import pluginVue from 'eslint-plugin-vue';
 import pluginStorybook from 'eslint-plugin-storybook';
+import globals from 'globals';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ['**/*.{js,vue}']
-  },
   {
     languageOptions: {
       globals: {
@@ -15,15 +12,41 @@ export default [
       },
     },
   },
+
   pluginJs.configs.recommended,
+  {
+    rules: {
+      'no-console': [
+        'error',
+        {
+          allow: ['warn', 'error', 'info'],
+        },
+      ],
+    },
+  },
+
   ...pluginVue.configs['flat/recommended'],
+  {
+    languageOptions: {
+      globals: {
+        defineProps: 'readonly',
+        defineEmits: 'readonly',
+        defineExpose: 'readonly',
+        defineModel: 'readonly',
+      },
+    },
+    rules: {
+      'vue/no-v-html': 'off',
+    },
+  },
+
   ...pluginStorybook.configs['flat/recommended'],
   {
     rules: {
       'vue/multi-word-component-names': 'off',
-    }
+    },
   },
   {
-    ignores: ['!.storybook']
+    ignores: ['!.storybook'],
   },
 ];
